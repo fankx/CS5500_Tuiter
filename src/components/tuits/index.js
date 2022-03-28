295328931f5ed27ee3feae1023f4956c9722ec15
Frankie Luo <luo.jingy@northeastern.edu>
2022-03-28 16:26:40 -0400

    implemented likes-button.

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -3,12 +3,14 @@ import './tuits.css';
 import Tuit from './tuit';
 import * as likesService from '../../services/likes-service';
 import * as service from '../../services/tuits-service';
+
 const Tuits = ({ tuits = [], refreshTuits }) => {
   const likeTuit = (tuit) =>
     likesService
       .userLikesTuit('me', tuit._id)
       .then(refreshTuits)
       .catch((e) => alert(e));
+
   const deleteTuit = (tid) => service.deleteTuit(tid).then(refreshTuits);
 
   return (
