b4795c3d6735702f36fab5f0c61d1ca85c948e51
Frankie Luo <luo.jingy@northeastern.edu>
2022-04-02 15:37:32 -0400

    Finished tests.

--- src/components/profile/my-dislikes.js
+++ src/components/profile/my-dislikes.js
@@ -1,6 +1,7 @@
 import Tuits from '../tuits';
 import * as service from '../../services/dislikes-service';
 import { useEffect, useState } from 'react';
+import SampleTuit from '../tuits/sampleTuit';
 
 const MyDislikes = () => {
   const [dislikedTuits, setDislikedTuits] = useState([]);
@@ -10,9 +11,22 @@ const MyDislikes = () => {
       .then((tuits) => setDislikedTuits(tuits));
   useEffect(findTuitsIDislike, []);
 
+  const MOCKED_TUIT = {
+    tuit: 'MOCKED TUIT',
+    postedBy: '1',
+    postedOn: '2000/01/01',
+    stats: {
+      replies: 1,
+      retuits: 1,
+      likes: 1,
+      dislikes: 1,
+    },
+  };
+
   return (
     <div>
       <Tuits tuits={dislikedTuits} refreshTuits={findTuitsIDislike} />{' '}
+      <SampleTuit tuit={MOCKED_TUIT}></SampleTuit>
     </div>
   );
 };
